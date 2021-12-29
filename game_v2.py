76df0957058af35510e298ced7a47abb6fd98c87
John Doe <johndoe@example.com>
2021-12-29 14:18:29 +0300

    v1

--- game_v2.py
+++ game_v2.py
@@ -1,26 +1,31 @@
+"""Угадай число
+Компьютер сам загадывает и угадывает число
+"""
+
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
-count = 0
+def random_predict(number: int=1) ->int:
+    """Рандомно угадываем число
 
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    Args:
+        number (int, optional): Загадываем число. Defaults to 1.
 
-    if predict_number > number:
-        print("Число должно быть меньше!")
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла, если угадали
+    return count
 
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
-    
-    
-    
-    
-    def score_game(random_predict) -> int:
+print(f'Количество попыток: {random_predict(10)}')
+
+def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
@@ -42,5 +47,6 @@ while True:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
-score_game(random_predict)
+if __name__== "__main__":
+    # RUN
+    score_game(random_predict)
